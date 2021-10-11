7d0cdb2e7f8d6dbb8edd582044c261dc629d5954
LeeMJ <Leeminjie1995@126.com>
2021-10-11 18:00:09 +0800

    适配器模式

---
+++ 设计模式/demo/src/main/java/adapter/example1/DC5.java
@@ -0,0 +1,8 @@
+package adapter.example1;
+
+/**
+ * ITarget
+ */
+public interface DC5 {
+    int output5();
+}
---
+++ 设计模式/demo/src/main/java/adapter/example1/Test.java
@@ -0,0 +1,8 @@
+package adapter.example1;
+
+public class Test {
+    public static void main(String[] args) {
+        PowerAdapter adapter = new PowerAdapter();
+        adapter.output5();
+    }
+}
