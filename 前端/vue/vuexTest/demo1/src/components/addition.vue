6c81882609fc14365fc7ac1ee68913f4fa1889d9
LeeMJ <Leeminjie1995@126.com>
2021-10-15 00:18:14 +0800

    vuexTest

---
+++ 前端/vue/vuexTest/demo1/src/components/addition.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>当前count值为:{{this.$store.state.count}}</h3>
+        <button @click="clickBtn">+</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {}
+        },
+
+        methods:{
+            clickBtn(){
+                this.$store.commit('addd');
+            }
+        }
+    }
+</script>
---
+++ 前端/vue/vuexTest/demo1/src/components/substra.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h3>当前count值为: {{count}}</h3>
+        <h3>msg: {{msg}}</h3>
+        <button>-</button>
+    </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+
+export default{
+    data(){
+        return {}
+    },
+
+    computed: {
+        ...mapState(['count','msg'])
+    }
+}
+</script>
