0ef2a057f382d3e1b8660cfb7538b882f090573e
xbmjml <Leeminjie1995@126.com>
2022-01-11 21:11:03 +0800

    test3

--- 并发/src/main/java/chapter4/MultiThread.java
+++ 并发/src/main/java/chapter4/MultiThread.java
@@ -1,7 +1,10 @@
 package chapter4;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
 public class MultiThread {
     public static void main(String[] args) {
-        
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
     }
 }
