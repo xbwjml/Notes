c69a6c4ff1bc2ca5df653a21b82ad1578adc57dc
xbwjml <1371726232@qq.com>
2025-07-13 20:50:37 +0800

    文件的物理结构

--- javatips/src/main/java/test0301/Test1223.java
+++ javatips/src/main/java/test0301/Test1223.java
@@ -1,4 +1,10 @@
 package test0301;
 
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingDeque;
+
 public class Test1223 {
+    public static void main(String[] args) {
+        Math.floorMod()
+    }
 }
