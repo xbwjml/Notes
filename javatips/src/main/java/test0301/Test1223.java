e3af312d1d236dff74dc90b9363b3826458d0f7b
xbwjml <1371726232@qq.com>
2025-07-17 01:15:45 +0800

    硬件的工作原理

--- javatips/src/main/java/test0301/Test1223.java
+++ javatips/src/main/java/test0301/Test1223.java
@@ -1,10 +1,51 @@
 package test0301;
 
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class Test1223 {
     public static void main(String[] args) {
-        Math.floorMod()
+        int[] capacity = new int[]{91,54,63,99,24,45,78};
+        int[] rocks = new int[]{35,32,45,98,6,1,25};
+        int additionalRocks = 17;
+
+        int[] ints = Arrays.copyOfRange(capacity, 7, 7);
+
+        System.out.println(ints);
+    }
+
+    public static int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+
+        int fullNum = 0;
+        List<Integer> list = new LinkedList<>();
+        int gap = 0;
+
+        for (int i=0; i<capacity.length; i++) {
+            int remains = capacity[i] - rocks[i];
+            if (remains == 0) {
+                fullNum++;
+                continue;
+            }
+            if (remains <= additionalRocks) {
+                list.add(remains);
+                gap += remains;
+                continue;
+            }
+        }
+
+        if (gap <= additionalRocks) {
+            return capacity.length;
+        }
+
+        Collections.sort(list);
+        for (Integer c : list) {
+            additionalRocks -= c;
+            if (additionalRocks < 0) {
+                break;
+            }
+            fullNum++;
+        }
+
+        return fullNum;
     }
 }
